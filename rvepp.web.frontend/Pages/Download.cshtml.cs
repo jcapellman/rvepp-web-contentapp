45e654e4cccbddf7f3fb51ea90f1dbf5c6d45cbf
Jarred Capellman <jcapellman@hotmail.com>
2024-07-13 22:22:06 -0500

    Added: Download page

---
+++ rvepp.web.frontend/Pages/Download.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace rvepp.web.frontend.Pages
+{
+    public class DownloadModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
