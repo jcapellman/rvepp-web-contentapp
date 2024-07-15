e941f7e3372eb04d6b039e07067e482116f19b08
Jarred Capellman <jcapellman@hotmail.com>
2024-07-15 07:15:51 -0500

    Added: Extension Method for converting a string to a SHA256 hash

---
+++ rvepp.web.frontend/Common/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace rvepp.web.frontend.Common
+{
+    public static class StringExtensions
+    {
+        public static string ToSha256(this string str) => BitConverter.ToString(SHA256.HashData(Encoding.ASCII.GetBytes(str))).Replace("-", "").ToUpper();
+    }
+}
