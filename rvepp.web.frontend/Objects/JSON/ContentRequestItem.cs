52a13af4d9d4722a6abc4bc289448b18439308f6
Jarred Capellman <jcapellman@hotmail.com>
2024-07-15 06:41:24 -0500

    Added: ContentRequestItem class

---
+++ rvepp.web.frontend/Objects/JSON/ContentRequestItem.cs
@@ -0,0 +1,13 @@
+﻿using rvepp.web.frontend.Enums;
+
+namespace rvepp.web.frontend.Objects.JSON
+{
+    public class ContentRequestItem
+    {
+        public ContentTypes ContentType { get; set; }
+
+        public required string Title { get; set; }
+
+        public required string Body { get; set; }
+    }
+}
