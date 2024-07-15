96329d4c0efe2920c9fb53a5581bb434de85b9f7
Jarred Capellman <jcapellman@hotmail.com>
2024-07-14 20:07:20 -0500

    Added: Content Table

---
+++ rvepp.web.frontend/Database/Tables/Content.cs
@@ -0,0 +1,14 @@
+﻿using rvepp.web.frontend.Database.Tables.Base;
+using rvepp.web.frontend.Enums;
+
+namespace rvepp.web.frontend.Database.Tables
+{
+    public class Content : BaseTable
+    {
+        public required string Title { get; set; }
+
+        public required ContentTypes ContentType { get; set; }
+
+        public required string Body { get; set; }
+    }
+}
