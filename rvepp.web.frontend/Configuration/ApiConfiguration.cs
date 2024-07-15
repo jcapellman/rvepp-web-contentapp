0999efb35a7af5b21f81485b132d520e264191c9
Jarred Capellman <jcapellman@hotmail.com>
2024-07-15 07:11:46 -0500

    Added: ApiConfiguration Class

---
+++ rvepp.web.frontend/Configuration/ApiConfiguration.cs
@@ -0,0 +1,15 @@
+﻿namespace rvepp.web.frontend.Configuration
+{
+    public class ApiConfiguration
+    {
+        public required string JWTSecret { get; set; }
+
+        public required string JWTAudience { get; set; }
+
+        public required string JWTIssuer { get; set; }
+
+        public required string JWTSubject { get; set; }
+
+        public required string JWTHashToken { get; set; }
+    }
+}
