eafa25a309d0e78c219334c664156bb1761c31e5
Jarred Capellman <jcapellman@hotmail.com>
2024-07-14 20:07:45 -0500

    Added: ContentTypes Enum

---
+++ rvepp.web.frontend/Enums/ContentTypes.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace rvepp.web.frontend.Enums
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum ContentTypes
+    {
+        News = 1,
+        Downloads = 2
+    }
+}
