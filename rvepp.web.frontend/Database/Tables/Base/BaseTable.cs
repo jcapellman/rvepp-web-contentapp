a8b35bab5450259f214d3b3532178693a0e2e7e4
Jarred Capellman <jcapellman@hotmail.com>
2024-07-15 06:42:34 -0500

    Added: Identity Column

--- rvepp.web.frontend/Database/Tables/Base/BaseTable.cs
+++ rvepp.web.frontend/Database/Tables/Base/BaseTable.cs
@@ -1,7 +1,12 @@
-﻿namespace rvepp.web.frontend.Database.Tables.Base
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace rvepp.web.frontend.Database.Tables.Base
 {
     public class BaseTable
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
         public int Id { get; set; }
 
         public DateTime Created { get; set; }
