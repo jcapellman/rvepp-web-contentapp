d83647697c5ef4762530bb01e0f78f23b7b4d8bf
Jarred Capellman <jcapellman@hotmail.com>
2024-07-14 20:07:34 -0500

    Added: Initial commit of the RveppDbContext

---
+++ rvepp.web.frontend/Database/RveppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using rvepp.web.frontend.Database.Tables;
+
+namespace rvepp.web.frontend.Database
+{
+    public class RveppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Content> Content { get; set; }
+    }
+}
