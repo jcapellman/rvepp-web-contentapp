dec27b4ab57960078648094f07cecfe7d6467e20
Jarred Capellman <jcapellman@hotmail.com>
2024-07-14 20:07:09 -0500

    Added: BaseTable class

---
+++ rvepp.web.frontend/Database/Tables/Base/BaseTable.cs
@@ -0,0 +1,13 @@
+﻿namespace rvepp.web.frontend.Database.Tables.Base
+{
+    public class BaseTable
+    {
+        public int Id { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime Modified { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
