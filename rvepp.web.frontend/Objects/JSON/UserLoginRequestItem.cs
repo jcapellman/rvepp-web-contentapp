f4aadb92bfb8ea166bebf468b6138edd238a5881
Jarred Capellman <jcapellman@hotmail.com>
2024-07-15 07:15:28 -0500

    Added: UserLoginRequestItem object

---
+++ rvepp.web.frontend/Objects/JSON/UserLoginRequestItem.cs
@@ -0,0 +1,9 @@
+﻿namespace rvepp.web.frontend.Objects.JSON
+{
+    public class UserLoginRequestItem
+    {
+        public required string UserName { get; set; }
+
+        public required string Password { get; set; }
+    }
+}
