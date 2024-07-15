7f346b406175218189d3933b38bef9f5d9da0477
Jarred Capellman <jcapellman@hotmail.com>
2024-07-14 22:21:12 -0500

    Added: Extension method to render the markdown

---
+++ rvepp.web.frontend/Common/MarkdownExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Html;
+
+namespace rvepp.web.frontend.Common
+{
+    public static class MarkdownExtension
+    {
+        public static HtmlString ToHtmlString(this string markdown)
+            => new(Markdig.Markdown.ToHtml(markdown));
+    }
+}
