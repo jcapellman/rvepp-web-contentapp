c899a6ad9e9cca2d109a92f573072c9c15630736
Jarred Capellman <jcapellman@hotmail.com>
2024-07-14 22:21:28 -0500

    Added: News Listing and handling no news posts

--- rvepp.web.frontend/Pages/News.cshtml.cs
+++ rvepp.web.frontend/Pages/News.cshtml.cs
@@ -1,12 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using rvepp.web.frontend.Database;
+using rvepp.web.frontend.Database.Tables;
+
 namespace rvepp.web.frontend.Pages
 {
-    public class NewsModel : PageModel
+    public class NewsModel(RveppDbContext dbContext) : PageModel
     {
+        public List<Content>? NewsPosts { get; private set; }
+
         public void OnGet()
         {
+            NewsPosts = [.. dbContext.Content.Where(a => a.ContentType == Enums.ContentTypes.News).OrderByDescending(a => a.Created)];
         }
     }
 }
