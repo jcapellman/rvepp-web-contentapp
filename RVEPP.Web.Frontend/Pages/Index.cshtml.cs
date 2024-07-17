bf2a3ae81ea1ff566bd960c823b015ad98352954
Jarred Capellman <jcapellman@hotmail.com>
2024-07-16 19:45:32 -0500

    Added: DB Query to get the latest news

--- RVEPP.Web.Frontend/Pages/Index.cshtml.cs
+++ RVEPP.Web.Frontend/Pages/Index.cshtml.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RVEPP.Web.Frontend.Database;
+using RVEPP.Web.Frontend.Database.Tables;
 
 namespace RVEPP.Web.Frontend.Pages
 {
-    public class IndexModel() : PageModel
+    public class IndexModel(RveppDbContext dbContext) : PageModel
     {
+        public Content? LatestNews { get; private set; }
+
         public void OnGet()
         {
-
+            LatestNews = dbContext.Content.Where(a => a.ContentType == Enums.ContentTypes.News && a.Active).OrderByDescending(a => a.Created).FirstOrDefault();
         }
     }
 }
