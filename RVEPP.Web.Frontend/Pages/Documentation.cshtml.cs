0a889bc6de7f02d4b42f8d60c089c3373214cc98
Jarred Capellman <jcapellman@hotmail.com>
2024-07-20 16:43:01 -0500

    Added: Conditional logic to display Documentation if available

--- RVEPP.Web.Frontend/Pages/Documentation.cshtml.cs
+++ RVEPP.Web.Frontend/Pages/Documentation.cshtml.cs
@@ -1,12 +1,16 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using RVEPP.Web.Frontend.Database;
+using RVEPP.Web.Frontend.Database.Tables;
+using RVEPP.Web.Frontend.Pages.Base;
 
 namespace RVEPP.Web.Frontend.Pages
 {
-    public class DocumentationModel : PageModel
+    public class DocumentationModel(RveppDbContext dbContext) : BasePageModel(dbContext)
     {
+        public Content? Documentation { get; private set; }
+
         public void OnGet()
         {
+            Documentation = GetLatestContentByType(Enums.ContentTypes.Documentation);
         }
     }
 }
