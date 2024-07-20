7acfc4bd613bdf6a3f3bb5fee53866694914bc11
Jarred Capellman <jcapellman@hotmail.com>
2024-07-20 16:35:56 -0500

    Added: BasePageModel class

---
+++ RVEPP.Web.Frontend/Pages/Base/BasePageModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using RVEPP.Web.Frontend.Database;
+using RVEPP.Web.Frontend.Database.Tables;
+
+namespace RVEPP.Web.Frontend.Pages.Base
+{
+    public class BasePageModel(RveppDbContext dbContext) : PageModel
+    {
+        protected Content? GetLatestContentByType(Enums.ContentTypes type) =>
+            dbContext.Content.Where(a => a.ContentType == type && a.Active).OrderByDescending(a => a.Created).FirstOrDefault();
+
+    }
+}
