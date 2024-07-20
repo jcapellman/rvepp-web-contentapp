20230bcccf1f3ab5d927c30db55a6f7a6b4c8168
Jarred Capellman <jcapellman@hotmail.com>
2024-07-20 16:40:56 -0500

    Added: Conditional logic to display Downloads if available

--- RVEPP.Web.Frontend/Pages/Download.cshtml.cs
+++ RVEPP.Web.Frontend/Pages/Download.cshtml.cs
@@ -1,12 +1,16 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using RVEPP.Web.Frontend.Database;
+using RVEPP.Web.Frontend.Database.Tables;
+using RVEPP.Web.Frontend.Pages.Base;
 
 namespace RVEPP.Web.Frontend.Pages
 {
-    public class DownloadModel : PageModel
+    public class DownloadModel(RveppDbContext dbContext) : BasePageModel(dbContext)
     {
+        public List<Content>? Downloads { get; private set; }
+
         public void OnGet()
         {
+            Downloads = [.. dbContext.Content.Where(a => a.ContentType == Enums.ContentTypes.Downloads && a.Active).OrderByDescending(a => a.Modified)];
         }
     }
 }
